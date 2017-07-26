6ffd331f91695b4f5eae99eee0850b3dd93f3a9a
itiu <ValeriyBushenev@gmail.com>
2017-07-26 14:52:42 +0300

    ++ type

---
+++ src/main/java/sm/tools/veda_client/Type.java
@@ -0,0 +1,11 @@
+package sm.tools.veda_client;
+
+public final class Type
+{
+	public static final int _Uri = 1;
+	public static final int _String = 2;
+	public static final int _Integer = 4;
+	public static final int _Datetime = 8;
+	public static final int _Decimal = 32;
+	public static final int _Bool = 64;
+}
